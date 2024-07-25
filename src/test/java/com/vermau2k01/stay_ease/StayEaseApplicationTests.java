435ed89c7a17caaeffe2fdda5994e262a7b867ec
Utkarsh <vermau2k01@gmail.com>
2024-07-25 22:02:19 +0530

    Initial Commit

---
+++ src/test/java/com/vermau2k01/stay_ease/StayEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vermau2k01.stay_ease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
